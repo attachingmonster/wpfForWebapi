7f5b7f037dd7908dcd64271b831a559d52915789
zhg12581 <2634562735@qq.com>
2019-05-26 21:15:35 +0800

    修改密码传输

--- wpfForWebapi/wpfForWebapi/MainWindow.xaml.cs
+++ wpfForWebapi/wpfForWebapi/MainWindow.xaml.cs
@@ -48,16 +48,16 @@ namespace wpfForWebapi
         }
         #endregion
 
+        #region 登录界面事件
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
         }
-
-        
-
+      
         private void loginChangePwd_Click(object sender, RoutedEventArgs e)
         {
-
+            LoginWindow.Visibility = Visibility.Collapsed;
+            ChangePasswordWindow.Visibility = Visibility.Visible;
         }
 
         private void loginetrievePwd_Click(object sender, RoutedEventArgs e)
@@ -82,9 +82,45 @@ namespace wpfForWebapi
             viewModelInformation = await LoginView(viewModelLogin);
         }
 
+
+        /// <summary>
+        /// 登录信息提交webapi
+        /// </summary>
+        private async Task<ViewModelInformation> LoginView(ViewModelLogin viewModelLogin)
+        {
+            //异常中断，程序不会破溃
+            ViewModelInformation viewModelInformation = null;
+            try
+            {
+                //Post异步提交信息，格式为Json
+                var response = await client.PostAsJsonAsync("http://localhost:60033/api/Login/PostLogin", viewModelLogin);
+                response.EnsureSuccessStatusCode();
+                viewModelInformation = await response.Content.ReadAsAsync<ViewModelInformation>();
+                if (viewModelInformation == null)
+                {
+                    viewModelInformation.Message = "网络错误";
+                    return viewModelInformation;
+                }
+                else
+                {
+                    return viewModelInformation;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                //后续保存到数据库里，另外再续返回到webapi的数据库里备查
+                viewModelInformation.Message = ex.Message;
+                return viewModelInformation;
+            }
+            catch (System.FormatException)
+            {
+                return viewModelInformation;
+            }
+        }
+
+        #endregion
+
         #region 注册账号界面
-
-
 
         private void loginRegister_Click(object sender, RoutedEventArgs e)    //切换界面事件
         {
@@ -234,11 +270,11 @@ namespace wpfForWebapi
                 var response = await client.PostAsJsonAsync("http://localhost:60033/api/Register/PostRegister", viewModelRegister);
                 response.EnsureSuccessStatusCode();
                 viewModelInformation = await response.Content.ReadAsAsync<ViewModelInformation>();
-                if (viewModelInformation== null)
+                if (viewModelInformation == null)
                 {
                     viewModelInformation.Message = "网络错误";
                     return viewModelInformation;
-                }       
+                }
                 else
                 {
                     return viewModelInformation;
@@ -255,19 +291,35 @@ namespace wpfForWebapi
                 return viewModelInformation;
             }
         }
+        #endregion
 
+        #region 修改密码界面
+        private async void ChangePsw_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModelChangePsw viewModelChangePsw = new ViewModelChangePsw();
+            viewModelChangePsw.Account = tbxUserAccountChangePwd.Text;
+            viewModelChangePsw.OldPassword = pbxOldPasswordChangePwd.Password;
+            viewModelChangePsw.NewPassword = pbxUserPasswordChangePwd.Password;
+            viewModelChangePsw.SurePassword = pbxSurePasswordChangePwd.Password;
+            ViewModelInformation viewModelInformation = new ViewModelInformation();
+            viewModelInformation = await ChangePswView(viewModelChangePsw);
 
-        /// <summary>
-        /// 登录信息提交webapi
-        /// </summary>
-        private async Task<ViewModelInformation> LoginView(ViewModelLogin viewModelLogin)
+        }
+
+        private void changePwdBack_click(object sender, RoutedEventArgs e)
+        {
+            ChangePasswordWindow.Visibility = Visibility.Collapsed;
+            LoginWindow.Visibility = Visibility.Visible;
+        }
+
+        private async Task<ViewModelInformation> ChangePswView(ViewModelChangePsw viewModelChangePsw)
         {
             //异常中断，程序不会破溃
             ViewModelInformation viewModelInformation = null;
             try
             {
                 //Post异步提交信息，格式为Json
-                var response = await client.PostAsJsonAsync("http://localhost:60033/api/Login/PostLogin", viewModelLogin);
+                var response = await client.PostAsJsonAsync("http://localhost:60033/api/ChangePsw/PostChangePsw", viewModelChangePsw);
                 response.EnsureSuccessStatusCode();
                 viewModelInformation = await response.Content.ReadAsAsync<ViewModelInformation>();
                 if (viewModelInformation == null)
@@ -291,10 +343,15 @@ namespace wpfForWebapi
                 return viewModelInformation;
             }
         }
+
         #endregion
+
+
         private void UserAccount_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        
     }
 }
---
+++ wpfForWebapi/wpfForWebapi/ViewModels/ViewModelChangePsw.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfForWebapi.ViewModels
+{
+    public class ViewModelChangePsw
+    {
+        public String Account { get; set; }
+
+        public String OldPassword { get; set; }
+        public String NewPassword { get; set; }
+        public String SurePassword { get; set; }
+    }
+}
